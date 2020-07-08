a5d4c9eedbd7acc1b9c5c843e79cd267c973673a
Ana Choi <danielaposso@yahoo.com>
2020-07-09 01:09:26 +0800

    adding basic js files

---
+++ basic-js/02-Sum-of-negative/lib/sum_of_negative.js
@@ -0,0 +1,19 @@
+sumOfNegative = (numbers) => {
+  // TODO: You are getting a `numbers` array. Return the sum of **negative** numbers only.
+}
+
+
+
+
+
+
+sumOfNegative = numbers => { sumOfNegative.forEach(sumOfNegative)<0+sumOfNegative.forEach(sumOfNegative)<0
+
+
+const beatles = ["paul", "john", "ringo", "george"];
+beatles.forEach((beatle) => {
+  console.log(beatle.toUpperCase());
+});
+
+
+if (num<0) + ()
