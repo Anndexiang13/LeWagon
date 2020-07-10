0caaa22218ff0acbb1d91bec121642b9bb1fbd7a
Ana Choi <danielaposso@yahoo.com>
2020-07-10 21:07:23 +0800

    adding select element exercise

---
+++ dom-events/01-Select-element/lib/select.js
@@ -0,0 +1,19 @@
+// This will check if you're able to run javascript.
+// If you don't see a popup greeting you, talk to a teacher!
+
+alert("Hello! You are running Javascript")
+
+// TODO 1. Open the `index.html` file and set an id attribute to the France <li>
+// (nothing to write in this file for this step, write it in the html file)
+
+
+// TODO 2: select the France list item and return it!
+
+const showChina = () => {
+  const selectcountry = document.querySelector("#country");
+  return selectcountry;
+}
+
+console.log(showChina())
+
+
